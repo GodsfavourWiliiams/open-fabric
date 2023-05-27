9f13b63cb58f734f974713e8bc9a6611d14f66b9
GodsfavourWiliiams <godzfeivvur@gmail.com>
2023-05-27 09:36:15 +0100

    4 files modified, 12 added

--- open-fabric/src/app/app.module.ts
+++ open-fabric/src/app/app.module.ts
@@ -1,22 +1,23 @@
 import { NgModule } from '@angular/core';
 import { BrowserModule } from '@angular/platform-browser';
-import { MatIconModule } from '@angular/material/icon';
 import { MatButtonModule } from '@angular/material/button';
 import { MatToolbarModule } from '@angular/material/toolbar';
 
 import { AppRoutingModule } from './app-routing.module';
 import { AppComponent } from './app.component';
 import { BrowserAnimationsModule } from '@angular/platform-browser/animations';
+import { ProductsListsComponent } from './products-lists/products-lists.component';
+import { ProductsDetailsComponent } from './products-details/products-details.component';
+import { AlertsComponent } from './alerts/alerts.component';
 
 @NgModule({
-  declarations: [AppComponent],
+  declarations: [AppComponent, ProductsListsComponent, ProductsDetailsComponent, AlertsComponent],
   imports: [
     BrowserModule,
     AppRoutingModule,
     BrowserAnimationsModule,
     MatToolbarModule,
     MatButtonModule,
-    MatIconModule,
   ],
   providers: [],
   bootstrap: [AppComponent],
---
+++ open-fabric/src/app/products-lists/products-lists.component.ts
@@ -0,0 +1,10 @@
+import { Component } from '@angular/core';
+
+@Component({
+  selector: 'app-products-lists',
+  templateUrl: './products-lists.component.html',
+  styleUrls: ['./products-lists.component.css']
+})
+export class ProductsListsComponent {
+
+}
